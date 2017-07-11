821b7f1b062d03470a30680b67138d5fe480dec0
Alejandro Yepes <alejandro@alejandros-macbook-pro.local>
2017-07-11 13:13:39 -0500

    created project and set up view controllers

---
+++ Give n Take/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  Give n Take
+//
+//  Created by Alejandro Yepes on 7/11/17.
+//  Copyright © 2017 Alejandro Yepes. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
